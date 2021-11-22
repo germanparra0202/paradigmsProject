a904e9787a74f229d1aac9075d138642828b6354
Sean Beckerle <sbeckerl@nd.edu>
2021-11-22 00:43:56 -0500

    Fixing urls.py error

--- taskTracker/taskTracker/urls.py
+++ taskTracker/taskTracker/urls.py
@@ -13,21 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
-
-from . import views
-
-app_name = "tasks"
+from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
-    # Index View 
-    path('',views.IndexView.as_view(), name='index'),   
-    # Features 1-6
-    #path('<int:question_id>/tasks/', views.firstSixFeaturesQuestions.as_view(), name='features'),
-    # Feature 7
-    path('<int:question_id>/taskSeven/', views.QuestionSevenView.as_view(), name='featureSeven'),
-    # Feature 8
-    path('<int:question_id>/taskEight/', views.QuestionEightView.as_view(), name='featureEight'),
-    # Results from features 
-    path('<int:question_id>/results/', views.ResultsView.as_view(), name='results')  
+    path('tasks/', include('tasks.urls')),
+    path('admin/', admin.site.urls),
 ]
