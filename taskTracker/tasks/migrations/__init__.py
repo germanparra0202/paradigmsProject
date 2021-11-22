cca3c4c1dc988687f1c20b11dc14b26564d22ab0
Sean Beckerle <sbeckerl@nd.edu>
2021-11-22 17:59:45 -0500

    Update __init__.py

--- taskTracker/tasks/migrations/__init__.py
+++ taskTracker/tasks/migrations/__init__.py
@@ -1,7 +1,7 @@
 from django.db import migrations, models
 import django.db.models.deletion
 
-class Migration(migrations.Migtation):
+class Migration(migrations.Migration):
     
     initial = True
 
