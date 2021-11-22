5f6886a2d741ab9d03222df20f96e588172ae75b
germanparra0202 <78463807+germanparra0202@users.noreply.github.com>
2021-11-22 10:56:28 -0500

    Update urls.py

--- taskTracker/tasks/urls.py
+++ taskTracker/tasks/urls.py
@@ -4,14 +4,14 @@ from . import views
 app_name = "tasks"
 
 urlpatterns = [
-    # Index View 
-    path('',views.IndexView.as_view(), name='index'),   
-    # Features 1-6
-    path('<int:question_id>/tasks/', views.firstSixFeaturesQuestions.as_view(), name='features'),
+    # First list view, main view 
+    path('index/', views.index, name='main-view'),
+    # Feature 1
+    path('featureone/', views.featureOne(), name='feature'),   
+    # Features 2-6
+    path('features/', views.feature2through6.as_view(), name='featurestwothroughsix'),
     # Feature 7
-    path('<int:question_id>/taskSeven/', views.QuestionSevenView.as_view(), name='featureSeven'),
+    path('featureseven/', views.featureSevenView.as_view(), name='featureSeven'),
     # Feature 8
-    path('<int:question_id>/taskEight/', views.QuestionEightView.as_view(), name='featureEight'),
-    # Results from features 
-    path('<int:question_id>/results/', views.ResultsView.as_view(), name='results')  
+    path('featureeight/', views.featureEightView.as_view(), name='featureEight') 
 ]
