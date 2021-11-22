cdda9bfef22c9c00dbe03c658106c69b3b1f8f6c
germanparra0202 <78463807+germanparra0202@users.noreply.github.com>
2021-11-21 21:52:38 -0500

    Update urls.py

--- taskTracker/taskTracker/urls.py
+++ taskTracker/taskTracker/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
 
+from . import views
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('',views.IndexView.as_view(), name='index'),                     # ex: /polls/
+    path('<int:question_id>/', views.QuestionDetailView.as_view(), name='detail'),              # ex: /polls/5/
+    path('<int:question_id>/results/', views.ResultsView.as_view(), name='results'),    # ex: /polls/5/results/
+    path('<int:question_id>/vote/', views.vote, name='vote'),             # ex: /polls/5/vote/
 ]
