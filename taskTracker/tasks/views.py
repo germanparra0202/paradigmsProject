4d6990caa032ebac7eec839a51677238a7e60f3e
Joshua Russell <jrusse22@nd.edu>
2021-11-22 11:08:15 -0600

    views update

--- taskTracker/tasks/views.py
+++ taskTracker/tasks/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
+from .models import Task, TimeEntry
+
+class MainView(ListView):
+    template_name = 'tasks/main.html'
+    context_object_name = 'tasks_list'
+
+    def get_taskset(self):
+        """Return a list of the user's tasks."""
+        return Task.objects
+
+class TaskDetailView(DetailView):
+    model = Task
+    template_name = 'tasks/detail.html'
+
+class TaskCreateView(CreateView):
+    model = Task
+    fields = 
 # Create your views here.
