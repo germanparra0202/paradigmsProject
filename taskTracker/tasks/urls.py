be27088d732a031f0472b6a5b71640872623470d
germanparra0202 <78463807+germanparra0202@users.noreply.github.com>
2021-11-22 17:47:09 -0500

    Update urls.py

--- taskTracker/tasks/urls.py
+++ taskTracker/tasks/urls.py
@@ -4,14 +4,10 @@ from . import views
 app_name = "tasks"
 
 urlpatterns = [
-    # First list view, main view 
-    path('index/', views.index, name='main-view'),
-    # Feature 1
-    path('featureone/', views.featureOne.as_view(), name='feature'),   
-    # Features 2-6
-    path('features/', views.feature2through6.as_view(), name='featurestwothroughsix'),
-    # Feature 7
-    path('featureseven/', views.featureSevenView.as_view(), name='featureSeven'),
-    # Feature 8
-    path('featureeight/', views.featureEightView.as_view(), name='featureEight') 
+    # Feature 1 --> Create
+    path('task/add/', views.TaskCreateView.as_view(), name='task-add'),
+    # Feature 2 --> Update
+    path('task/<int:pk>/', views.TaskUpdateView.as_view(), name='task-update'),
+    # Feature 3 --> Delete 
+    path('task/<int:pk>/delete/', views.TaskDeleteView.as_view(), name='task-delete')
 ]
