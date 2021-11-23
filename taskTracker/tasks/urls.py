3aae51009e397e6b8b3e8478f40f78aa86e2a9d9
Sean Beckerle <sbeckerl@nd.edu>
2021-11-22 19:42:46 -0500

    Updates

--- taskTracker/tasks/urls.py
+++ taskTracker/tasks/urls.py
@@ -5,11 +5,11 @@ app_name = "tasks"
 
 urlpatterns = [
     # Main Window 
-    path('task/', views.MainView.as_view(), name='main-view'),
+    path('', views.MainView.as_view(), name='main-view'),
     # Feature 1 --> Create
-    path('task/add/', views.TaskCreateView.as_view(), name='task-add'),
+    path('tasks/add/', views.TaskCreateView.as_view(), name='task-add'),
     # Feature 2 --> Update
-    path('task/<int:pk>/', views.TaskUpdateView.as_view(), name='task-update'),
+    path('tasks/<int:pk>/', views.TaskUpdateView.as_view(), name='task-update'),
     # Feature 3 --> Delete 
-    path('task/<int:pk>/delete/', views.TaskDeleteView.as_view(), name='task-delete')
+    path('tasks/<int:pk>/delete/', views.TaskDeleteView.as_view(), name='task-delete')
 ]
