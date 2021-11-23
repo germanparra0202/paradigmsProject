142a1b0ecc4f3901143e1aeec1943a111c0987aa
Sean Beckerle <sbeckerl@nd.edu>
2021-11-23 01:36:31 -0500

    Setup Views

--- taskProject/tasks/urls.py
+++ taskProject/tasks/urls.py
@@ -5,5 +5,5 @@ app_name = 'tasks'
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-
+    path('<int:pk>', views.SingleView.as_view(), name='single')
 ]
--- taskProject/tasks/views.py
+++ taskProject/tasks/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Core
+from django.views.generic import ListView, DetailView, UpdateView
 
 # Create your views here.
+class IndexView(ListView):
+    model = Task
+    template_name = 'tasks/index.html'
+    context_object_name = 'index'
+
+class SingleView(DetailView):
+    model = Task
+    template_name = 'tasks/single.html'
+    context_object_name = 'task'
