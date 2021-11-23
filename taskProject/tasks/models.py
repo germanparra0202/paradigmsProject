0cff590b3e16894e518daf2e1b1e20d15a6330f3
Joshua Russell <jrusse22@nd.edu>
2021-11-23 00:56:36 -0600

    initial

--- taskProject/tasks/models.py
+++ taskProject/tasks/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Task(models.Model):
+    name = models.CharField(max_length=200)
+    desc = models.CharField(max_length=200)
+    proj = models.CharField(max_length=200)
+    status = models.BooleanField(default=True)
+
+    def get_absolute_url(self):
+        return reverse('tasks:single', args=[self.id])
+    
+    class Meta:
+        ordering = ['-id']
+
+    def __str__(self):
+        return self.name
+
