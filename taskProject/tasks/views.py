a91f3eebc9a9ec03a97efc3d396a96d63f8bb085
Joshua Russell <jrusse22@nd.edu>
2021-11-23 01:46:56 -0600

    admin

--- taskProject/tasks/views.py
+++ taskProject/tasks/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Core
+from .models import Task
 from django.views.generic import ListView, DetailView, UpdateView
 
 # Create your views here.
