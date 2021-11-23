162687f82711a87669af5dc972bcdde6e94fad98
germanparra0202 <78463807+germanparra0202@users.noreply.github.com>
2021-11-22 19:07:00 -0500

    Update urls.py

--- taskTracker/tasks/urls.py
+++ taskTracker/tasks/urls.py
@@ -4,6 +4,8 @@ from . import views
 app_name = "tasks"
 
 urlpatterns = [
+    # Main Window 
+    path('task/', views.MainView.as_view(), name='main-view'),
     # Feature 1 --> Create
     path('task/add/', views.TaskCreateView.as_view(), name='task-add'),
     # Feature 2 --> Update
