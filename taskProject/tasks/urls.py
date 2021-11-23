d5f4838bf2c986bb42ef08edf0b8a7cc7335fb26
Joshua Russell <jrusse22@nd.edu>
2021-11-23 00:58:00 -0600

    initial

--- taskProject/tasks/urls.py
+++ taskProject/tasks/urls.py
@@ -1,9 +1,9 @@
- from django.urls import path
- from . import views
+from django.urls import path
+from . import views
 
- app_name = 'tasks'
+app_name = 'tasks'
 
- urlpatterns = [
-     #path('', views.IndexView.as_view(), name='index'),
+urlpatterns = [
+
 
  ]
