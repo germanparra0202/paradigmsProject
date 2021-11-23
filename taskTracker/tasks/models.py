6a126be9e56cff721a225cd541fadc725f1aad11
Sean Beckerle <sbeckerl@nd.edu>
2021-11-22 21:26:00 -0500

    Changes made

--- taskTracker/tasks/models.py
+++ taskTracker/tasks/models.py
@@ -5,8 +5,7 @@ class Task(models.Model):
     name_text = models.CharField(max_length=200)
     desc_text = models.CharField(max_length=200)
     proj_text = models.CharField(max_length=200)
-    status    = models.BooleanField(default=True)
-    
+    status    = models.BooleanField(default=True)  
 
     def __str__(self):
         return f"{self.id}: {self.name_text}"
